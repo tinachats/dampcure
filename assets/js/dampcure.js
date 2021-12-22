4e8de099b3cf091899bcaa0c5659dcaaef17c731
tinachats <marshallchaterera@gmail.com>
2021-12-22 06:21:14 +0200

    Team Cards complete

--- assets/js/dampcure.js
+++ assets/js/dampcure.js
@@ -25,8 +25,8 @@ $(function() {
         nav: false,
         dots: true,
         autoplay: true,
-        autoplay: true,
         smartSpeed: 450,
+        responsiveClass: true,
         responsive: {
             0: {
                 items: 1
@@ -36,6 +36,27 @@ $(function() {
             },
             1000: {
                 items: 3
+            }
+        }
+    });
+
+    $('#designs-carousel').owlCarousel({
+        loop: true,
+        margin: 0,
+        nav: false,
+        dots: false,
+        autoplay: false,
+        smartSpeed: 450,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 2
+            },
+            1000: {
+                items: 5
             }
         }
     });
---
+++ main.py
@@ -0,0 +1,4 @@
+def userData(first_name, last_name, age):
+    print('Your full name is ' + first_name + ' ' + last_name + '. You are aged', age)
+
+userData('tinashe', 'mapuranga', 45)
